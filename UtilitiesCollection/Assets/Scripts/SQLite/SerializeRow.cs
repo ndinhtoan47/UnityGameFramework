15c7b9b0268152e376bb777079f2f39808d4b2ef
Nguyễn Đinh Toàn <ndinhtoan48@gmail.com>
2018-12-03 16:41:48 +0700

    SQLite demo

---
+++ UtilitiesCollection/Assets/Scripts/SQLite/SerializeRow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Serialize.Row
+{
+    [System.Serializable]
+    public class SerializeRow : ISerializable
+    {
+        private List<object> list;
+
+        public int Count { get { return list.Count; } }
+
+        public object this[int index]
+        {
+            get { return list[index]; }
+            set { list[index] = value; }
+        }
+
+        public SerializeRow(int capacity)
+        {
+            list = new List<object>(capacity);
+        }
+
+        public SerializeRow(SerializationInfo info, StreamingContext context)
+        {
+            list = (List<object>)info.GetValue("fieldValues", typeof(List<object>));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("fieldValues", list, typeof(List<object>));
+        }
+
+        public void Add(object item)
+        {
+            list.Add(item);
+        }
+
+        public bool Remove(object item)
+        {
+            return list.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        public int IndexOf(object item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public bool Contains(object item)
+        {
+            return list.Contains(item);
+        }
+    }
+}
